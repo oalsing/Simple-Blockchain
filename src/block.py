adc117876c334664e65438e1784e24412883f1e5
oalsing <oscar.alsing@gmail.com>
2017-11-12 12:15:39 +0100

    removes merkle_tree for later implementation, and adds data and static genesis method

--- src/block.py
+++ src/block.py
@@ -3,25 +3,26 @@ import hashlib
 
 
 class Block:
-    def __init__(self, previous_block_hash, merkle_root, timestamp):
+    def __init__(self, previous_block_hash, data, timestamp):
         self.previous_block_hash = previous_block_hash
-        self.merkle_root = merkle_root
+        self.data = data
         self.timestamp = timestamp
         self.hash = self.get_hash()
 
-    def create_genesis_block(self):
+    @staticmethod
+    def create_genesis_block():
         return Block("0", "0", datetime.datetime.now())
 
     def create_next_block(self, previous_block):
         prev_block_hash = previous_block.hash
-        merkle_root = ""
+        data = ""
         timestamp = datetime.datetime.now()
 
-        return Block(prev_block_hash, merkle_root, timestamp)
+        return Block(prev_block_hash, data, timestamp)
 
     def get_hash(self):
         header_bin = (str(self.previous_block_hash) +
-                      str(self.merkle_root) +
+                      str(self.data) +
                       str(self.timestamp))
 
         inner_hash = hashlib.sha256(header_bin.encode()).hexdigest().encode()
